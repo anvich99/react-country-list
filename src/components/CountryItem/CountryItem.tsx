6d33e96215cc1c96bdda398f924be3a9fe02b263
Nastya <zhitnikovich3308@gmail.com>
2023-03-21 12:30:33 +0300

    fix: Rename variables

--- src/components/CountryItem/CountryItem.tsx
+++ src/components/CountryItem/CountryItem.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 import { Country } from '../../types/types'
 import { Badge } from '../Badge/Badge'
 interface CountryItemProps {
-  info: Country,
+  infoCountry: Country,
 }
-export const CountryItem = ({info}: CountryItemProps) => {
+export const CountryItem = ({infoCountry}: CountryItemProps) => {
   return (
     <li className ="list-group-item d-flex justify-content-between align-items-center">
-      <img  className = "w-25" src={info.flag} alt="flag" />
-      <p className ="text-start fs-3">{info.name}</p>
-      <p className ="text-start fs-3">{info.capital}</p>
-      <p className ="text-start fs-3">{info.region}</p>
-      <Badge text = 'area' color = 'primary' value = {info.area} />
-      <Badge text = 'population' color = 'secondary' value = {info.population}/>
+      <img  className = "w-25" src={infoCountry.flag} alt="flag" />
+      <p className ="text-start fs-3">{infoCountry.name}</p>
+      <p className ="text-start fs-3">{infoCountry.capital}</p>
+      <p className ="text-start fs-3">{infoCountry.region}</p>
+      <Badge text = 'area' color = 'primary' value = {infoCountry.area} />
+      <Badge text = 'population' color = 'secondary' value = {infoCountry.population}/>
     </li>
   )
 }
